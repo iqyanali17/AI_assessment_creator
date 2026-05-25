c9c078ae16c238b82c1606e4dd3c08ceefb105ae
iqyanali17 <khwajaiqyanali@gmail.com>
2026-05-25 23:21:20 +0530

    tsc-alias fix

--- backend/src/routes/health.route.ts
+++ backend/src/routes/health.route.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getHealth } from '@/controllers/health.controller';
+import { getHealth } from "../controllers/health.controller";
 
 const router = Router();
 
